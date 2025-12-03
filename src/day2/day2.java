bea889cbb924cbc84189ba7748f990c17fafb57e
Yasmina Husein <yasminah@chalmers.se>
2025-12-03 21:25:23 +0100

    day 2

--- src/day2/day2.java
+++ src/day2/day2.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class day2 {
     public static void main(String[] args) {
-        int count = 0;
+        long count = 0;
         int end = 0;
         File puzzle = new File("day-2-puzzle-input.txt");
         try (Scanner reader = new Scanner(puzzle)) {
@@ -15,22 +15,22 @@ public class day2 {
                 while (reader.hasNextLine()) {
                     code = reader.nextLine();
                 }
-                while (end < code.length()) {
+                while (code.contains(",")) {
                     end = code.indexOf(",");
-                    code = code.substring(end+1, code.length());
                     String range = code.substring(0, end);
+                    code = code.substring(end+1);
                     int line = range.indexOf("-");
-                    int first = Integer.parseInt(range.substring(0, line));
-                    int last = Integer.parseInt(range.substring(line + 1, end));
-                    for (int i = first; i <= last; i++) {
-                        String check = Integer.toString(i);
+                    long first = Long.parseLong(range.substring(0, line));
+                    long last = Long.parseLong(range.substring(line + 1,end));
+                    for (long i = first; i <= last; i++) {
+                        String check = Long.toString(i);
                         if (check.length() % 2 == 0) {
                             if (check.length() == 2) {
                                 if (i % 11 == 0) {
-                                    count += i;
+                                    count +=  i;
                                 }
                             }
-                            if (check.substring(0, ((check.length() / 2) - 1)).equals(check.substring((check.length() / 2), (check.length()) - 1))) {
+                            else if (check.substring(0, ((check.length() / 2))).equals(check.substring((check.length()/2)))) {
                                 count += i;
                             }
                         }
