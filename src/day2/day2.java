054eaa9ba22762136a71b9c78b00e90c487852a5
Yasmina Husein <yasminah@chalmers.se>
2025-12-04 19:17:07 +0100

    day 2

--- src/day2/day2.java
+++ src/day2/day2.java
@@ -7,7 +7,10 @@ import java.util.Scanner;
 public class day2 {
     public static void main(String[] args) {
         long count = 0;
-        int end = 0;
+        int end;
+        long first;
+        long last;
+        int line;
         File puzzle = new File("day-2-puzzle-input.txt");
         try (Scanner reader = new Scanner(puzzle)) {
             {
@@ -18,30 +21,39 @@ public class day2 {
                 while (code.contains(",")) {
                     end = code.indexOf(",");
                     String range = code.substring(0, end);
-                    code = code.substring(end+1);
-                    int line = range.indexOf("-");
-                    long first = Long.parseLong(range.substring(0, line));
-                    long last = Long.parseLong(range.substring(line + 1,end));
+                    code = code.substring(end + 1);
+                    line = range.indexOf("-");
+                    first = Long.parseLong(range.substring(0, line));
+                    last = Long.parseLong(range.substring(line + 1, end));
                     for (long i = first; i <= last; i++) {
                         String check = Long.toString(i);
                         if (check.length() % 2 == 0) {
-                            if (check.length() == 2) {
-                                if (i % 11 == 0) {
-                                    count +=  i;
-                                }
-                            }
-                            else if (check.substring(0, ((check.length() / 2))).equals(check.substring((check.length()/2)))) {
+                            if (check.substring(0, ((check.length() / 2)))
+                                    .equals(check.substring((check.length() / 2)))) {
                                 count += i;
                             }
                         }
                     }
                 }
+                line = code.indexOf("-");
+                first = Long.parseLong(code.substring(0, line));
+                last = Long.parseLong(code.substring(line + 1));
+                for (long i = first; i <= last; i++) {
+                    String check = Long.toString(i);
+                    if (check.length() % 2 == 0) {
+                        if (check.substring(0, ((check.length() / 2)))
+                                .equals(check.substring((check.length() / 2)))) {
+                            count += i;
+                        }
+                    }
+                }
+                System.out.println(count);
             }
-        System.out.println(count);
-    }
+        }
         catch(FileNotFoundException e){
-            System.out.println("An error occurred.");
-            e.printStackTrace();
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+            }
         }
     }
-}
+
